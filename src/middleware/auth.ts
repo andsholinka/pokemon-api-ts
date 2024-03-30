a4b60d297b967699afb71c19017471f300c21a1f
Andrey Sholinka <sholinka2111@gmail.com>
2024-03-30 14:22:08 +0700

    auth jwt

---
+++ src/middleware/auth.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import GeneralHelper from "../helpers/GeneralHelper";
+
+const Auth = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if (token === undefined) {
+            return res.status(401).send(GeneralHelper.ResponseData(401, "Unauthorized", null, null));
+        }
+
+        const userData = GeneralHelper.ExtractToken(token);
+
+        if (!userData) {
+            return res.status(401).send(GeneralHelper.ResponseData(401, "Unauthorized", null, null));
+        }
+
+        res.locals.userId = userData?.id
+        next();
+
+    } catch (error: any) {
+        return res.status(500).send(GeneralHelper.ResponseData(500, "Internal Server Error", error, null));
+    }
+}
+
+export default Auth
